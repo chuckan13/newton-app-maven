5cc806182766223bb636d72ba3f71786a156df6c
PlatGod <an.charlie@gmail.com>
2020-05-26 16:07:07 -0400

    check principal

--- src/main/java/com/example/LoginuserController.java
+++ src/main/java/com/example/LoginuserController.java
@@ -1,9 +1,11 @@
 package com.example;
 
+import java.security.Principal;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -16,11 +18,19 @@ public class LoginuserController {
         this.repository = repository;
     }
 
+    // @PreAuthorize("#id == principal.user.id")
     @RequestMapping(value = "/{id}", method = RequestMethod.GET)
-    public ResponseEntity<Loginuser> get(@PathVariable("id") Long id) {
+    public ResponseEntity<Loginuser> get(@PathVariable("id") Long id, Principal principal) {
+        System.out.println(principal.getName());
         Loginuser user = repository.findOne(id);
         if (null == user)
             return new ResponseEntity<Loginuser>(HttpStatus.NOT_FOUND);
+
+        String userName = user.getUserName();
+        System.out.println(userName);
+        if (!userName.equals(principal.getName())) {
+            return new ResponseEntity<Loginuser>(HttpStatus.FORBIDDEN);
+        }
         return new ResponseEntity<Loginuser>(user, HttpStatus.OK);
     }
 
@@ -34,7 +44,8 @@ public class LoginuserController {
     }
 
     @RequestMapping(value = "/{id}", method = RequestMethod.PATCH)
-    public ResponseEntity<Loginuser> editUser(@PathVariable("id") Long id, @RequestBody Loginuser loginuser) {
+    public ResponseEntity<Loginuser> editUser(@PathVariable("id") Long id, @RequestBody Loginuser loginuser,
+            Principal principal) {
         Loginuser foundLoginuser = repository.findOne(id);
         if (null == foundLoginuser)
             return new ResponseEntity<Loginuser>(HttpStatus.NOT_FOUND);
@@ -45,14 +56,14 @@ public class LoginuserController {
             }
             foundLoginuser.updateParameters(loginuser);
             repository.save(foundLoginuser);
-            return get(foundLoginuser.getId());
+            return get(foundLoginuser.getId(), principal);
         }
     }
 
     @RequestMapping(value = "/new", method = RequestMethod.POST)
-    public ResponseEntity<Loginuser> update(@RequestBody Loginuser user) {
+    public ResponseEntity<Loginuser> update(@RequestBody Loginuser user, Principal principal) {
         repository.save(user);
-        return get(user.getId());
+        return get(user.getId(), principal);
     }
 
     @RequestMapping
