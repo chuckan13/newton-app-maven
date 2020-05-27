778bb7e16075cda27e530cf2772663a1fd60626b
Jorge Zreik <jorgezreik@Jorges-MacBook-Pro.local>
2020-05-27 19:40:05 -0400

    Made registration form responsive.

--- src/main/js/views/register.js
+++ src/main/js/views/register.js
@@ -9,6 +9,173 @@ import Button from "react-bootstrap/Button";
 
 import "../app.scss";
 
+import { useFormik } from "formik";
+import * as Yup from "yup";
+
+const validationSchema = Yup.object().shape({
+    firstName: Yup.string().required("Required"),
+    lastName: Yup.string().required("Required"),
+    email: Yup.string().email("Must be a valid email").required("Required"),
+    password: Yup.string()
+        .min(8, "Must be at least 8 characters")
+        .required("Required"),
+    confirmPassword: Yup.string()
+        .required("Required")
+        .oneOf([Yup.ref("password"), null], "Passwords must match"),
+    terms: Yup.bool().oneOf([true], "You must agree before submitting"),
+});
+
+function RegistrationForm() {
+    const {
+        handleSubmit,
+        handleChange,
+        values,
+        errors,
+        touched,
+        handleBlur,
+        dirty,
+        isValid,
+    } = useFormik({
+        initialValues: {},
+        validationSchema,
+        onSubmit(values) {
+            console.log(values);
+        },
+    });
+
+    return (
+        <React.Fragment>
+            <Form
+                noValidate
+                onSubmit={handleSubmit}
+                className="text-left floating-form mb-5"
+            >
+                <h2 className="text-center mb-4">
+                    <b>Register</b>
+                </h2>
+                <Form.Row>
+                    <Form.Group as={Col} md="6" className="pb-2">
+                        <Form.Label>First name</Form.Label>
+                        <Form.Control
+                            type="text"
+                            name="firstName"
+                            value={values.firstName}
+                            onChange={handleChange}
+                            onBlur={handleBlur}
+                            placeholder="First name"
+                            isValid={touched.firstName && !errors.firstName}
+                            isInvalid={touched.firstName && !!errors.firstName}
+                        />
+                        <Form.Control.Feedback
+                            type="invalid"
+                            className="position-absolute"
+                        >
+                            {errors.firstName}
+                        </Form.Control.Feedback>
+                    </Form.Group>
+                    <Form.Group as={Col} md="6" className="pb-2">
+                        <Form.Label>Last name</Form.Label>
+                        <Form.Control
+                            type="text"
+                            name="lastName"
+                            value={values.lastName}
+                            onChange={handleChange}
+                            onBlur={handleBlur}
+                            placeholder="Last name"
+                            isValid={touched.lastName && !errors.lastName}
+                            isInvalid={touched.lastName && !!errors.lastName}
+                        />
+                        <Form.Control.Feedback
+                            type="invalid"
+                            className="position-absolute"
+                        >
+                            {errors.lastName}
+                        </Form.Control.Feedback>
+                    </Form.Group>
+                </Form.Row>
+                <Form.Group className="pb-2">
+                    <Form.Label>Email</Form.Label>
+                    <Form.Control
+                        type="email"
+                        name="email"
+                        value={values.email}
+                        onChange={handleChange}
+                        onBlur={handleBlur}
+                        placeholder="Email"
+                        isValid={touched.email && !errors.email}
+                        isInvalid={touched.email && !!errors.email}
+                    />
+                    <Form.Control.Feedback
+                        type="invalid"
+                        className="position-absolute"
+                    >
+                        {errors.email}
+                    </Form.Control.Feedback>
+                </Form.Group>
+                <Form.Group className="pb-2">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control
+                        type="password"
+                        name="password"
+                        value={values.password}
+                        onChange={handleChange}
+                        onBlur={handleBlur}
+                        placeholder="Password"
+                        isValid={touched.password && !errors.password}
+                        isInvalid={touched.password && !!errors.password}
+                    />
+                    <Form.Control.Feedback
+                        type="invalid"
+                        className="position-absolute"
+                    >
+                        {errors.password}
+                    </Form.Control.Feedback>
+                </Form.Group>
+                <Form.Group className="pb-2">
+                    <Form.Label>Confirm password</Form.Label>
+                    <Form.Control
+                        type="password"
+                        name="confirmPassword"
+                        value={values.confirmPassword}
+                        onChange={handleChange}
+                        onBlur={handleBlur}
+                        placeholder="Confirm password"
+                        isValid={
+                            touched.confirmPassword && !errors.confirmPassword
+                        }
+                        isInvalid={
+                            touched.confirmPassword && !!errors.confirmPassword
+                        }
+                    />
+                    <Form.Control.Feedback
+                        type="invalid"
+                        className="position-absolute"
+                    >
+                        {errors.confirmPassword}
+                    </Form.Control.Feedback>
+                </Form.Group>
+                <Form.Group className="pb-2">
+                    <Form.Check
+                        required
+                        name="terms"
+                        label="I agree to terms and conditions"
+                        value={values.terms}
+                        onChange={handleChange}
+                        isInvalid={!!errors.terms}
+                        feedback={errors.terms}
+                        id="terms"
+                    />
+                </Form.Group>
+                <Row className="justify-content-center">
+                    <Button type="submit" variant="main" disabled={!(isValid && dirty)}>
+                        Submit
+                    </Button>
+                </Row>
+            </Form>
+        </React.Fragment>
+    );
+}
+
 class Register extends Component {
     render() {
         return (
@@ -16,34 +183,7 @@ class Register extends Component {
                 <div>
                     <NavBar />
                     <Col lg={4} md={5} sm={7} className="mx-auto mt-5">
-                        <Form className="floating-form mb-5">
-                            <h2 className="text-center mb-4">
-                                <b>Register</b>
-                            </h2>
-                            <Form.Group controlId="email">
-                                <Form.Label>Email address</Form.Label>
-                                <Form.Control type="email" />
-                            </Form.Group>
-                            <Form.Group controlId="password">
-                                <Form.Label>Password</Form.Label>
-                                <Form.Control type="password" />
-                            </Form.Group>
-                            <Form.Group controlId="confirmPassword">
-                                <Form.Label>Confirm password</Form.Label>
-                                <Form.Control type="password" />
-                            </Form.Group>
-                            <Form.Group controlId="agreeToTerms">
-                                <Form.Check
-                                    type="checkbox"
-                                    label="I agree to Terms and Policy."
-                                />
-                            </Form.Group>
-                            <Row className="justify-content-center">
-                                <Button variant="main" type="submit">
-                                    Submit
-                                </Button>
-                            </Row>
-                        </Form>
+                        <RegistrationForm />
                     </Col>
                 </div>
                 <Footer />
