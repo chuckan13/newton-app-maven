a88ae2d3876705c021d75bd9eeedb1c7c6dcd05c
PlatGod <an.charlie@gmail.com>
2020-06-02 19:00:02 -0400

    login

--- src/main/java/com/example/WebSecurityConfig.java
+++ src/main/java/com/example/WebSecurityConfig.java
@@ -41,15 +41,16 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
 
-                // .csrf().disable().authorizeRequests().antMatchers("/login").permitAll().anyRequest().authenticated()
-                // .and().formLogin().loginPage("/login").permitAll().and().logout().invalidateHttpSession(true)
-                // .clearAuthentication(true).logoutRequestMatcher(new
-                // AntPathRequestMatcher("/logout"))
-                // .logoutSuccessUrl("/logout-success").permitAll();
+                .csrf().disable().authorizeRequests().antMatchers("/login").permitAll().anyRequest().authenticated()
+                .and().formLogin().loginPage("/login").permitAll().and().logout().invalidateHttpSession(true)
+                .clearAuthentication(true).logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+                .logoutSuccessUrl("/logout-success").permitAll();
 
-                .httpBasic().and().authorizeRequests().antMatchers(HttpMethod.GET, "/**").hasRole("USER").and()
-                .authorizeRequests().antMatchers(HttpMethod.PATCH, "/**").hasRole("USER").and().csrf().disable()
-                .formLogin().disable();
+        // .httpBasic().and().authorizeRequests().antMatchers(HttpMethod.GET,
+        // "/**").hasRole("USER").and()
+        // .authorizeRequests().antMatchers(HttpMethod.PATCH,
+        // "/**").hasRole("USER").and().csrf().disable()
+        // .formLogin().disable();
 
     }
 }
