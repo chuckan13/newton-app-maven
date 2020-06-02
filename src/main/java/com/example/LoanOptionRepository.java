543fa66c336843a5cc67a5a1de39da66cb8948dc
PlatGod <an.charlie@gmail.com>
2020-06-02 16:11:17 -0400

    loanoptino repo fix

--- src/main/java/com/example/LoanOptionRepository.java
+++ src/main/java/com/example/LoanOptionRepository.java
@@ -4,6 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface LoanRepository extends JpaRepository<Loan, Long> {
+public interface LoanOptionRepository extends JpaRepository<LoanOption, Long> {
 
 }
