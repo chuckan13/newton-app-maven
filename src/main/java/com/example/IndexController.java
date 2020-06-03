d98d4c7a7683ed7ac840506f7b12c9f0fa847431
PlatGod <an.charlie@gmail.com>
2020-06-02 21:01:55 -0400

    login

--- src/main/java/com/example/IndexController.java
+++ src/main/java/com/example/IndexController.java
@@ -8,20 +8,20 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class IndexController {
-    @RequestMapping(value = "/") //
-    public String index(Map<String, Object> model) {
-        return "index.html";
-    }
+    // @RequestMapping(value = "/") //
+    // public String index(Map<String, Object> model) {
+    // return "index.html";
+    // }
     // @RequestMapping("/")
     // @ResponseBody
     // public String home() {
     // return "Welcome to home page";
     // }
 
-    @RequestMapping("/login")
-    public String loginPage(Map<String, Object> model) {
-        return "login.js";
-    }
+    // @RequestMapping("/login")
+    // public String loginPage(Map<String, Object> model) {
+    // return "login.js";
+    // }
 
     // @RequestMapping("/logout-success")
     // public String logoutPage(Map<String, Object> model) {
--- src/main/java/com/example/MvcConfiguration.java
+++ src/main/java/com/example/MvcConfiguration.java
@@ -1,24 +1,19 @@
-// package com.example;
+package com.example;
 
-// import org.springframework.context.annotation.ComponentScan;
-// import org.springframework.context.annotation.Configuration;
-// import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-// import
-// org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
-// import
-// org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
-// import org.springframework.web.servlet.view.InternalResourceViewResolver;
-// import org.springframework.web.servlet.view.JstlView;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
 
-// @Configuration
-// @ComponentScan
-// public class MvcConfiguration extends WebMvcConfigurerAdapter {
-// @Override
-// public void configureViewResolvers(ViewResolverRegistry registry) {
-// InternalResourceViewResolver resolver = new InternalResourceViewResolver();
-// resolver.setPrefix("/static/");
-// resolver.setSuffix(".html");
-// resolver.setViewClass(JstlView.class);
-// registry.viewResolver(resolver);
-// }
-// }
+@ComponentScan
+@Configuration
+public class MvcConfiguration extends WebMvcConfigurerAdapter {
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("index");
+        registry.addViewController("/login").setViewName("index");
+    }
+}
