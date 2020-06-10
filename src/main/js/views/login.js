efb568a749a6cd0bc2e970ca35fee387a6bba4e8
Jorge Zreik <jfzreik@gmail.com>
2020-06-09 21:44:48 -0500

    Login form changed to component.

--- src/main/js/views/login.js
+++ src/main/js/views/login.js
@@ -17,121 +17,131 @@ const validationSchema = Yup.object().shape({
     password: Yup.string().required("Required"),
 });
 
-function LoginForm() {
-    let loginFailed = null;
-    const {
-        handleSubmit,
-        handleChange,
-        values,
-        errors,
-        touched,
-        handleBlur,
-        dirty,
-        isValid,
-        isSubmitting,
-    } = useFormik({
-        initialValues: {
-            username: "",
-            password: "",
-        },
-        validationSchema,
-        onSubmit(values) {
-            let formData = new FormData();
-            formData.append("username", values.username);
-            formData.append("password", values.password);
-            const data = new URLSearchParams(formData);
-            // const data = JSON.stringify({
-            // 	fullName: 'Niko Fotopoulos',
-            // 	userName: values.username,
-            // 	password: values.password,
-            // 	role: 'USER',
-            // 	loanOption1: '',
-            // 	loanOption2: '',
-            // 	loanOption3: '',
-            // 	stripeCustomerId: '',
-            // 	autopay: false,
-            // 	selectedLoan: 0
-            // });
-            // const loginData = JSON.stringify(values);
-            // console.log('LOG IN VALUES');
-            // console.log(formData);
-            // console.log(data);
-            // debugger;
-            fetch("https://newton-server-maven.herokuapp.com/login-process", {
-                method: "POST",
-                body: data,
-            })
-                .then((response) => {
-                    console.log("Success");
-                    // console.log(response.headers.get('Authorization'));
-                    if (response.ok) {
-                        window.location.replace(response.url);
-                    } else {
-                        loginFailed = (
-                            <div className="invalid-feedback d-block">
-                                Your email or password is incorrect.
-                            </div>
-                        );
+class LoginForm extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { loginFailed: null };
+    }
+
+    render() {
+        const {
+            handleSubmit,
+            handleChange,
+            values,
+            errors,
+            touched,
+            handleBlur,
+            dirty,
+            isValid,
+            isSubmitting,
+        } = useFormik({
+            initialValues: {
+                username: "",
+                password: "",
+            },
+            validationSchema,
+            onSubmit(values) {
+                let formData = new FormData();
+                formData.append("username", values.username);
+                formData.append("password", values.password);
+                const data = new URLSearchParams(formData);
+                // const data = JSON.stringify({
+                // 	fullName: 'Niko Fotopoulos',
+                // 	userName: values.username,
+                // 	password: values.password,
+                // 	role: 'USER',
+                // 	loanOption1: '',
+                // 	loanOption2: '',
+                // 	loanOption3: '',
+                // 	stripeCustomerId: '',
+                // 	autopay: false,
+                // 	selectedLoan: 0
+                // });
+                // const loginData = JSON.stringify(values);
+                // console.log('LOG IN VALUES');
+                // console.log(formData);
+                // console.log(data);
+                // debugger;
+                fetch(
+                    "https://newton-server-maven.herokuapp.com/login-process",
+                    {
+                        method: "POST",
+                        body: data,
                     }
-                })
-                .catch((error) => {
-                    console.error("Error:", error);
-                });
-        },
-    });
-    return (
-        <Form
-            noValidate
-            onSubmit={handleSubmit}
-            className="text-left floating-form mb-5"
-        >
-            <h2 className="text-center mb-4">
-                <b>Login</b>
-            </h2>
-            <Form.Group className="pb-2">
-                <Form.Label>Email</Form.Label>
-                <Form.Control
-                    type="email"
-                    name="username"
-                    value={values.username}
-                    onChange={handleChange}
-                    onBlur={handleBlur}
-                    placeholder="Email"
-                    isValid={touched.username && !errors.username}
-                    isInvalid={touched.username && !!errors.username}
-                />
-                <Form.Control.Feedback type="invalid">
-                    {errors.username}
-                </Form.Control.Feedback>
-            </Form.Group>
-            <Form.Group className="pb-2">
-                <Form.Label>Password</Form.Label>
-                <Form.Control
-                    type="password"
-                    name="password"
-                    value={values.password}
-                    onChange={handleChange}
-                    onBlur={handleBlur}
-                    placeholder="Password"
-                    isValid={touched.password && !errors.password}
-                    isInvalid={touched.password && !!errors.password}
-                />
-                <Form.Control.Feedback type="invalid">
-                    {errors.password}
-                </Form.Control.Feedback>
-            </Form.Group>
-            <Row className="justify-content-center pb-3">
-                <Button
-                    type="submit"
-                    variant="main"
-                    disabled={!(isValid && dirty) || isSubmitting}
-                >
-                    {isSubmitting ? "Loading..." : "Submit"}
-                </Button>
-                {loginFailed}
-            </Row>
-        </Form>
-    );
+                )
+                    .then((response) => {
+                        console.log("Success");
+                        // console.log(response.headers.get('Authorization'));
+                        if (response.ok) {
+                            window.location.replace(response.url);
+                        } else {
+                            loginFailed = (
+                                <div className="invalid-feedback d-block">
+                                    Your email or password is incorrect.
+                                </div>
+                            );
+                        }
+                    })
+                    .catch((error) => {
+                        console.error("Error:", error);
+                    });
+            },
+        });
+        
+        return (
+            <Form
+                noValidate
+                onSubmit={handleSubmit}
+                className="text-left floating-form mb-5"
+            >
+                <h2 className="text-center mb-4">
+                    <b>Login</b>
+                </h2>
+                <Form.Group className="pb-2">
+                    <Form.Label>Email</Form.Label>
+                    <Form.Control
+                        type="email"
+                        name="username"
+                        value={values.username}
+                        onChange={handleChange}
+                        onBlur={handleBlur}
+                        placeholder="Email"
+                        isValid={touched.username && !errors.username}
+                        isInvalid={touched.username && !!errors.username}
+                    />
+                    <Form.Control.Feedback type="invalid">
+                        {errors.username}
+                    </Form.Control.Feedback>
+                </Form.Group>
+                <Form.Group className="pb-2">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control
+                        type="password"
+                        name="password"
+                        value={values.password}
+                        onChange={handleChange}
+                        onBlur={handleBlur}
+                        placeholder="Password"
+                        isValid={touched.password && !errors.password}
+                        isInvalid={touched.password && !!errors.password}
+                    />
+                    <Form.Control.Feedback type="invalid">
+                        {errors.password}
+                    </Form.Control.Feedback>
+                </Form.Group>
+                <Row className="justify-content-center pb-3">
+                    <Button
+                        type="submit"
+                        variant="main"
+                        disabled={!(isValid && dirty) || isSubmitting}
+                    >
+                        {isSubmitting ? "Loading..." : "Submit"}
+                    </Button>
+                    {loginFailed}
+                </Row>
+            </Form>
+        );
+    }
 }
 
 class Login extends Component {
