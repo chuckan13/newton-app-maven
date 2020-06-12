cdd015955f5b4b72b85ad8e7e5f4f776a686d1b9
PlatGod <an.charlie@gmail.com>
2020-06-12 14:04:00 -0400

    response controller encryption/decryption

--- src/main/java/com/example/ResponseController.java
+++ src/main/java/com/example/ResponseController.java
@@ -5,6 +5,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.security.crypto.encrypt.Encryptors;
+import org.springframework.security.crypto.encrypt.TextEncryptor;
+import org.springframework.security.crypto.keygen.KeyGenerators;
 
 @RestController
 @RequestMapping("/api/responses")
@@ -21,11 +24,28 @@ public class ResponseController {
         Response response = repository.findOne(id);
         if (null == response)
             return new ResponseEntity<Response>(HttpStatus.NOT_FOUND);
-        return new ResponseEntity<Response>(response, HttpStatus.OK);
+        Response decryptedResponse = new Response();
+        decryptedResponse.updateParameters(response);
+        String response_secret_key = "2o6qK89ebqLAquMAgyOS4qV38aSu1WTBhhNgDUc9Tx8jBdVovq2yfGmxFh72OBy"; // change to
+                                                                                                        // heroku config
+                                                                                                        // vars
+        TextEncryptor decryptor = Encryptors.text(response_secret_key, decryptedResponse.getSalt());
+        String decryptedText = decryptor.decrypt(decryptedResponse.getText());
+        decryptedResponse.setText(decryptedText);
+        return new ResponseEntity<Response>(decryptedResponse, HttpStatus.OK);
     }
 
     @RequestMapping(value = "/new", method = RequestMethod.POST)
     public ResponseEntity<Response> update(@RequestBody Response response) {
+        String oldText = response.getText();
+        String response_secret_key = "2o6qK89ebqLAquMAgyOS4qV38aSu1WTBhhNgDUc9Tx8jBdVovq2yfGmxFh72OBy"; // change to
+                                                                                                        // heroku config
+                                                                                                        // var
+        String newSalt = KeyGenerators.string().generateKey();
+        TextEncryptor encryptor = Encryptors.text(response_secret_key, newSalt);
+        String encryptedText = encryptor.encrypt(oldText);
+        response.setText(encryptedText);
+        response.setSalt(newSalt);
         repository.save(response);
         return get(response.getId());
     }
