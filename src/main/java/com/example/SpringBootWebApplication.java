cc01406a94002e68af0eccf015d5727c28c3b8e8
PlatGod <an.charlie@gmail.com>
2020-06-28 17:09:43 -0400

    added bean

--- src/main/java/com/example/SpringBootWebApplication.java
+++ src/main/java/com/example/SpringBootWebApplication.java
@@ -1,11 +1,15 @@
 package com.example;
 
+import java.util.Locale;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.CookieLocaleResolver;
 
 @SpringBootApplication
 public class SpringBootWebApplication extends SpringBootServletInitializer {
@@ -18,6 +22,13 @@ public class SpringBootWebApplication extends SpringBootServletInitializer {
     @Bean
     public BCryptPasswordEncoder bCryptPasswordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public LocaleResolver localeResolver() {
+        final CookieLocaleResolver cookieLocaleResolver = new CookieLocaleResolver();
+        cookieLocaleResolver.setDefaultLocale(Locale.ENGLISH);
+        return cookieLocaleResolver;
     }
 
     public static void main(String[] args) throws Exception {
