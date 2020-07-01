a8a3e478e72f3da714f62f8a2b879d265e2484bd
ilene1129 <ilenee@outlook.com>
2020-06-30 21:37:04 -0400

    refactor loan options

--- src/main/js/views/loanoptions.js
+++ src/main/js/views/loanoptions.js
@@ -16,93 +16,6 @@ const validationSchema = Yup.object().shape({
     // loanoption: Yup.bool().oneOf([1, 2, 3], "You must choose an option"),
 });
 
-function LoanOptionsForm(props) {
-    const {
-        handleSubmit,
-        handleChange,
-        values,
-        errors,
-        touched,
-        handleBlur,
-        dirty,
-        isValid,
-        isSubmitting,
-        setSubmitting,
-    } = useFormik({
-        initialValues: {
-            loanoption: null,
-        },
-        validationSchema,
-        onSubmit(values) {
-            const data = JSON.stringify({
-                loanoption: values.loanoption
-            });
-            console.log(data);
-            fetch(
-                "https://newton-server-maven.herokuapp.com/",
-                {
-                    method: "POST",
-                    headers: {
-                        "Content-Type": "application/json",
-                        Accept: "application/json",
-                    },
-                    body: data,
-                }
-            )
-                .then((data) => {
-                    console.log("Test:", data);
-                    props.onFinish(true);
-                })
-                .then(() => {
-                    setSubmitting(false);
-                })
-                .catch((error) => {
-                    console.error("Error:", error);
-                });
-        },
-    });
-
-    return (
-        <React.Fragment>
-            <Form
-                onSubmit={handleSubmit}
-                className="text-left floating-form mb-5"
-            >
-                <Form.Group className="pb-2">
-                    <Form.Label>Loan Option</Form.Label>
-                    <Form.Control
-                        as="select"
-                        name="loanoption"
-                        value={values.loanoption}
-                        onChange={handleChange}
-                        onBlur={handleBlur}
-                        isValid={touched.loanoption && !errors.loanoption}
-                        isInvalid={touched.loanoption && !!errors.loanoption}
-                        defaultValue="Choose..."
-                    />
-                        <option value="1">{props.options.op1}</option>
-                        <option value="2">{props.options.op2}</option>
-                        <option value="3">{props.options.op3}</option>
-                    <Form.Control.Feedback type="invalid">
-                        {errors.loanoption}
-                    </Form.Control.Feedback>
-                </Form.Group>
-
-                <Row className="justify-content-center text-center">
-                    <Button
-                        type="submit"
-                        variant="main"
-                        disabled={!(isValid && dirty) || isSubmitting}
-                        className="mt-5"
-                    >
-                        {isSubmitting ? "Loading..." : "Submit"}
-                    </Button>
-                </Row>
-            </Form>
-        </React.Fragment>
-    );
-}
-
 class LoanOptions extends Component {
 
     constructor() {
@@ -124,12 +37,91 @@ class LoanOptions extends Component {
     }
 
     render() {
+        const {
+            handleSubmit,
+            handleChange,
+            values,
+            errors,
+            touched,
+            handleBlur,
+            dirty,
+            isValid,
+            isSubmitting,
+            setSubmitting,
+        } = useFormik({
+            initialValues: {
+                loanoption: null,
+            },
+            validationSchema,
+            onSubmit(values) {
+                const data = JSON.stringify({
+                    loanoption: values.loanoption
+                });
+                console.log(data);
+                fetch(
+                    "https://newton-server-maven.herokuapp.com/",
+                    {
+                        method: "POST",
+                        headers: {
+                            "Content-Type": "application/json",
+                            Accept: "application/json",
+                        },
+                        body: data,
+                    }
+                )
+                    .then((data) => {
+                        console.log("Test:", data);
+                        props.onFinish(true);
+                    })
+                    .then(() => {
+                        setSubmitting(false);
+                    })
+                    .catch((error) => {
+                        console.error("Error:", error);
+                    });
+            },
+        });
+
         return (
             <React.Fragment>
                 <div>
                     <NavBar />
                     <Col lg={4} md={5} sm={7} className="mx-auto mt-4">
-                        <LoanOptionsForm />
+                        <Form
+                            onSubmit={handleSubmit}
+                            className="text-left floating-form mb-5"
+                        >
+                            <Form.Group className="pb-2">
+                                <Form.Label>Loan Option</Form.Label>
+                                <Form.Control
+                                    as="select"
+                                    name="loanoption"
+                                    value={values.loanoption}
+                                    onChange={handleChange}
+                                    onBlur={handleBlur}
+                                    isValid={touched.loanoption && !errors.loanoption}
+                                    isInvalid={touched.loanoption && !!errors.loanoption}
+                                    defaultValue="Choose..."
+                                />
+                                <option value="1">{props.options.op1}</option>
+                                <option value="2">{props.options.op2}</option>
+                                <option value="3">{props.options.op3}</option>
+                                <Form.Control.Feedback type="invalid">
+                                    {errors.loanoption}
+                                </Form.Control.Feedback>
+                            </Form.Group>
+
+                            <Row className="justify-content-center text-center">
+                                <Button
+                                    type="submit"
+                                    variant="main"
+                                    disabled={!(isValid && dirty) || isSubmitting}
+                                    className="mt-5"
+                                >
+                                    {isSubmitting ? "Loading..." : "Submit"}
+                                </Button>
+                            </Row>
+                        </Form>
                     </Col>
                 </div>
                 <Footer />
